505da1cbc75221e27adc347a47d90f5858cbc30a
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-01-04 09:30:36 -0300

    feat: implementacao do exercicio 4

--- Ex04/Ex04.js
+++ Ex04/Ex04.js
@@ -6,3 +6,12 @@ considerando as seguintes taxas de câmbio:
 Imprima na tela o valor em reais inserido pelo usuário e suas
 conversões para dólar americano e euro.*/
 
+
+let valorReal = Number(prompt("Digite um valor em reais para ser convertido: "));
+
+let euro = valorReal / 6.1;
+let dolar = valorReal / 5.7;
+
+console.log(`R$${valorReal.toFixed(2)} é equivalente à €${euro.toFixed(2)} e a $${dolar.toFixed(2)}.`);
+
+
