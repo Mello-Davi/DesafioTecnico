d638516195d9378d6615988bdd37843aaa16cdde
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-01-04 10:17:59 -0300

    feat: criacao da pasta e das ppaginas html e js do exercicio 8

---
+++ Ex08/Ex08.js
@@ -0,0 +1,3 @@
+/*Implemente uma função chamada countBooksInCategory que conte o
+número de livros em cada categoria e retorne um objeto com o nome
+da categoria como chave e a contagem de livros como valor.*/
