a7ccd5c6d215e0f48fe05e09b321bb59565e823b
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-01-04 14:10:46 -0300

    feat: implementacao do exercicio 12

--- Ex12/Ex12.js
+++ Ex12/Ex12.js
@@ -3,3 +3,23 @@ um array de números inteiros onde cada elemento aparece exatamente
 duas vezes, exceto por um elemento que aparece exatamente uma vez.
 A função deve retornar o elemento que aparece apenas uma vez. Tal
 elemento deve ser impresso na tela.*/
+
+function encontrarElementoUnico(array) {
+    for (let i = 0; i < array.length; i++) {
+      let contadorDeAparicoes = 0;
+      
+      for (let j = 0; j < array.length; j++) {
+        if (array[i] === array[j]) {
+          contadorDeAparicoes++;
+        }
+      }
+      if (contadorDeAparicoes === 1) {
+        return array[i];
+      }
+    }
+    return undefined;
+  }
+  
+  let numerosInteiros = [2, 3, 5, 4, 5, 3, 4];
+  let unico = encontrarElementoUnico(numerosInteiros);
+  console.log(`Elemento único: ${unico}`);
