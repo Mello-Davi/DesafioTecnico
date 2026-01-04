d62a142c5e629191a6c6a240dca0b31aacf4e3c0
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-01-04 09:27:45 -0300

    feat: criacao da pasta e das paginas do exercicio 4

---
+++ Ex04/Ex04.js
@@ -0,0 +1,8 @@
+/*Elabore um algoritmo para ler um valor monetário em reais (R$) e
+convertê-lo para as moedas euro (€) e dólar americano (US$),
+considerando as seguintes taxas de câmbio:
+● Taxa de câmbio para euro: €1 = R$6,10
+● Taxa de câmbio para dólar: $1 = R$5,70
+Imprima na tela o valor em reais inserido pelo usuário e suas
+conversões para dólar americano e euro.*/
+
