3e7cf213a3e26093bc7eb61e329fda6512a80790
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-01-04 11:40:27 -0300

    feat: criacao da pasta e das paginas html e js do exercicio 11

---
+++ Ex11/Ex11.js
@@ -0,0 +1,4 @@
+/*Escreva um programa que receba uma lista de números inteiros e
+armazene-os em um array. O programa deve verificar quantos números
+do vetor estão em ordem crescente em relação ao número anterior e
+exibir essa informação. */
