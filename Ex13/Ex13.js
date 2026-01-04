0a95b4640da4ab799a67e3b95cc568e3abebed14
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-01-04 14:14:05 -0300

    feat: criacao de pasta e das paginas html e js do exercicio 13

---
+++ Ex13/Ex13.js
@@ -0,0 +1,7 @@
+/*Você está desenvolvendo um sistema para gerenciar jogadores de um
+time de esportes. Cada jogador deve ter as seguintes propriedades:
+● Nome (string)
+● Idade (número)
+● Posição (string)
+● Pontuação (número)
+Implemente as seguintes funcionalidades:.*/
