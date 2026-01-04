967ad44e995b468bc0b3bdebe63d8cb9d4c5ab80
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-01-04 10:16:30 -0300

    feat: implementacao do exercicio 7

--- Ex07/Ex07.js
+++ Ex07/Ex07.js
@@ -4,3 +4,27 @@ esquerda para a direita e da direita para a esquerda.
 Requisitos:
 Dica: Use toLowerCase() e split().*/
 
+//1. Solicite ao usuário uma frase.
+let frase = prompt("Digite uma frase para verificar se é um palíndromo:");
+
+//2. Remova todos os espaços e ignore a diferença entre letras maiúsculas e minúsculas.
+let arr = frase.toLowerCase().split(/\s+/);
+let str = "";
+for (let i = 0; i < arr.length; i++) {
+  str += arr[i];
+}
+
+//3. Verifique se a frase é igual quando lida ao contrário e informe se ela é um palíndromo ou não.
+let palindromo = true;
+for (let i = 0; i < str.length / 2; i++) {
+  if (str[i] !== str[str.length - 1 - i]) {
+    palindromo = false;
+    break;
+  }
+}
+
+if (palindromo) {
+  console.log("É um palíndromo! ");
+} else {
+  console.log("Não é um palíndromo.");
+}
