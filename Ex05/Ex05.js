4b4d945248d3970a5e84a912fa2eee7ae4ce2cde
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-01-04 09:36:50 -0300

    feat: implementacao do exercicio 5

--- Ex05/Ex05.js
+++ Ex05/Ex05.js
@@ -5,3 +5,13 @@ inteiro e realize o seguinte:
 ● Caso o valor seja divisível por 3 e 5, ao mesmo tempo, imprima
 “fizzbuzz”.
 ● Caso contrário imprima nada. */
+
+let inteiro = Number(prompt("Digite um número inteiro: "));
+
+if (inteiro % 3 === 0 && inteiro % 5 === 0 ){
+    console.log("fizzbuzz");
+} else if ( inteiro % 3 === 0){ 
+    console.log("fizz");
+} else if (inteiro % 5 === 0){
+    console.log("buzz");
+}
