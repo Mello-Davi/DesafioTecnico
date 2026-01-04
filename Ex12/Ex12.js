358eb2d77ec67fd86f44b2d3cb29868dceeeedf7
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-01-04 12:12:25 -0300

    feat: criacao da pasta e das paginas html e js do exercicio 12

---
+++ Ex12/Ex12.js
@@ -0,0 +1,5 @@
+/*Implemente uma função chamada encontrarElementoUnico que recebe
+um array de números inteiros onde cada elemento aparece exatamente
+duas vezes, exceto por um elemento que aparece exatamente uma vez.
+A função deve retornar o elemento que aparece apenas uma vez. Tal
+elemento deve ser impresso na tela.*/
