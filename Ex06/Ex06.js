4425377dfb5bfdb35b373713e88321684704a81e
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-01-04 09:38:24 -0300

    feat: criacao da pasta e das paginas do exercicio 6

---
+++ Ex06/Ex06.js
@@ -0,0 +1,11 @@
+/*Escreva um programa que:
+1. Solicite ao usuário um número inteiro positivo n, que representa o
+número de termos que ele deseja ver da sequência de Fibonacci.
+2. Implemente uma função recursiva chamada fibonacci que recebe
+um número n e retorna os n primeiros termos da sequência de
+Fibonacci, onde:
+○ Fibonacci(0) = 0
+
+○ Fibonacci(1) = 1
+○ Para n>1: Fibonacci(n) = Fibonacci(n-1) + Fibonacci(n-2)
+3. Exiba os n primeiros termos da sequência de Fibonacci.*/
