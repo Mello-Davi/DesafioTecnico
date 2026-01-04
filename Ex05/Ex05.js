33a7f5658bd21cd1bd46434670cc8d950fee1ac3
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-01-04 09:32:36 -0300

    feat: criacao da pasta e das paginas do exercicio 5

---
+++ Ex05/Ex05.js
@@ -0,0 +1,7 @@
+/*Elabore um algoritmo que peça para o usuário inserir um número
+inteiro e realize o seguinte:
+● Caso o valor seja divisível por 3, imprima “fizz”.
+● Caso o valor seja divisível por 5 imprima “buzz”.
+● Caso o valor seja divisível por 3 e 5, ao mesmo tempo, imprima
+“fizzbuzz”.
+● Caso contrário imprima nada. */
