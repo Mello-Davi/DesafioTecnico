794c188c926439fef4a403e9f4f91d9556b0e08a
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-01-04 10:50:23 -0300

    feat: criacao da pasta e das paginas de html e js do exercicio 10

---
+++ Ex10/Ex10.js
@@ -0,0 +1,14 @@
+/*Escreva um programa para simular uma fila de espera em uma central
+de atendimento ao cliente. O programa deve iniciar exibindo na tela um
+menu interativo contendo a lista de todos os clientes esperando
+atendimento, mostrando a posição ao lado do nome (ex.: 1o João, 2o
+Maria, etc).
+O menu deve permitir escolher entre as seguintes opções:
+1 - “Novo Cliente”: Adiciona um novo cliente ao final da fila
+(solicitando o nome do cliente).
+2 - “Atender Cliente”: Retira o primeiro cliente da fila e exibe o
+nome do cliente que está sendo atendido.
+3 - “Sair”: Encerra o programa.
+O programa só deve ser encerrado ao escolher a opção 3. Caso
+contrário, ele deve retornar ao menu principal.*/
+
