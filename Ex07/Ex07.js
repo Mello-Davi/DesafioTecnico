3704dac2ab729cbedb7d5865c5c649c92f54415d
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-01-04 09:57:14 -0300

    feat: criacao da pasta e das paginas do exercicio 7

---
+++ Ex07/Ex07.js
@@ -0,0 +1,6 @@
+/*Escreva um programa que verifique se uma frase é um palíndromo. Um
+palíndromo é uma sequência que pode ser lida da mesma maneira da
+esquerda para a direita e da direita para a esquerda.
+Requisitos:
+Dica: Use toLowerCase() e split().*/
+
